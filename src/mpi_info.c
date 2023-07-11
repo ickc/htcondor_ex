09fa2643ef12aff94d4ad56e7a89b12b579e8d66
Kolen Cheung <khcheung@wn5916090.in.tier2.hep.manchester.ac.uk>
2023-07-11 15:30:12 +0100

    add mpi_info.c

---
+++ src/mpi_info.c
@@ -0,0 +1,44 @@
+// Author: Wes Kendall
+// Copyright 2011 www.mpitutorial.com
+// This code is provided freely with the tutorials on mpitutorial.com. Feel
+// free to modify it for your own use. Any distribution of the code must
+// either provide a link to www.mpitutorial.com or keep this header intact.
+//
+// An intro MPI hello world program that uses MPI_Init, MPI_Comm_size,
+// MPI_Comm_rank, MPI_Finalize, and MPI_Get_processor_name.
+//
+#include <mpi.h>
+#include <sched.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc, char** argv) {
+  // Initialize the MPI environment. The two arguments to MPI Init are not
+  // currently used by MPI implementations, but are there in case future
+  // implementations might need the arguments.
+  MPI_Init(&argc, &argv);
+
+  // Get the number of processes
+  int world_size;
+  MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+  // Get the rank of the process
+  int world_rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+  // Get the name of the processor
+  char processor_name[MPI_MAX_PROCESSOR_NAME];
+  int name_len;
+  MPI_Get_processor_name(processor_name, &name_len);
+
+  int cpu = sched_getcpu();
+
+  long number_of_processors = sysconf(_SC_NPROCESSORS_ONLN);
+
+  // Print off a hello world message
+  printf("MPI rank %d (/%d) say hello from CPU %d (/%d), and processor %s\n",
+         world_rank, world_size, cpu, number_of_processors, processor_name);
+
+  // Finalize the MPI environment. No more MPI calls can be made after this
+  MPI_Finalize();
+}
