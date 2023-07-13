ab6962484dcc451c5521b9c8cf93b0a8ed9b941e
Kolen Cheung <christian.kolen@gmail.com>
2023-07-13 12:11:32 +0100

    emit more debug info: adapt from debug_repl

--- examples/cbatch-debug-mpi/env.sh
+++ examples/cbatch-debug-mpi/env.sh
@@ -1,4 +1,46 @@
-#!/usr/bin/env bash
+#!/bin/bash -l
 
-# this setup the environment, including OpenMPI
+# helpers ##############################################################
+
+COLUMNS=72
+
+print_double_line() {
+	eval printf %.0s= '{1..'"${COLUMNS}"\}
+	echo
+}
+
+print_line() {
+	eval printf %.0s- '{1..'"${COLUMNS}"\}
+	echo
+}
+
+########################################################################
+
+print_double_line
+echo "HTCondor config summary:"
+print_line
+condor_config_val -summary
+
+print_double_line
+echo "Current environment:"
+print_line
+env
+
+print_double_line
+echo "Avaiable MPI:"
+module avail mpi 2>&1
+
 module load mpi/openmpi3-x86_64
+
+print_double_line
+echo "Environment after loading OpenMPI:"
+print_line
+env
+
+print_double_line
+echo "module path:"
+which mpicc
+which mpirun
+
+echo stdout from $_CONDOR_PROCNO
+echo stderr from $_CONDOR_PROCNO >&2
