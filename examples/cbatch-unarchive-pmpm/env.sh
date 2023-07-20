179bee8baccee33a9a6383ed27f50dafa76b0b51
Kolen Cheung <christian.kolen@gmail.com>
2023-07-20 12:11:01 +0100

    add cbatch-unarchive-pmpm

---
+++ examples/cbatch-unarchive-pmpm/env.sh
@@ -0,0 +1,40 @@
+#!/bin/bash -l
+
+# helpers ##############################################################
+
+COLUMNS=72
+
+print_double_line() {
+	eval printf %.0s= '{1..'"${COLUMNS}"\}
+	echo
+}
+
+print_line() {
+	eval printf %.0s- '{1..'"${COLUMNS}"\}
+	echo
+}
+
+########################################################################
+
+print_double_line
+echo "HTCondor config summary:"
+print_line
+condor_config_val -summary
+
+print_double_line
+echo "Current environment:"
+print_line
+env
+
+print_double_line
+echo "Unarchive environment..."
+tar -xzf pmpm-20230718-Linux-x86_64-OpenMPI-ucx.tar.gz -C /tmp
+
+print_double_line
+echo "Activate environment..."
+source /tmp/pmpm-20230718/bin/activate /tmp/pmpm-20230718
+print_line
+echo "Python is available at:"
+which python
+echo "mpirun is available at:"
+which mpirun
---
+++ examples/cbatch-unarchive-pmpm/mpi.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+# helpers ##############################################################
+
+COLUMNS=72
+
+print_double_line() {
+	eval printf %.0s= '{1..'"${COLUMNS}"\}
+	echo
+}
+
+print_line() {
+	eval printf %.0s- '{1..'"${COLUMNS}"\}
+	echo
+}
+
+########################################################################
+
+print_double_line
+set_OMPI_HOST_one_slot_per_condor_proc
+echo "Running mpirun with host configuration: $OMPI_HOST" >&2
+
+# print_double_line
+echo "Running TOAST tests..."
+mpirun -v -host "$OMPI_HOST" python -c "import toast.tests; toast.tests.run()"
+
+# seems to not able to run 2 mpirun in a job
+# print_double_line
+# echo "Running TOAST benchmarks..."
+# mpirun -v -host "$OMPI_HOST" python /tmp/pmpm-20230718/bin/toast_benchmark.py
