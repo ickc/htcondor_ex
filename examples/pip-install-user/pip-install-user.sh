86c9bcaa80a4fe2bcc150ffc2d8bfff7934c9805
Kolen Cheung <christian.kolen@gmail.com>
2024-01-26 15:45:53 +0000

    add pip-install-user

---
+++ examples/pip-install-user/pip-install-user.sh
@@ -0,0 +1,55 @@
+#!/bin/bash -l
+
+# helpers ##############################################################
+
+COLUMNS=72
+
+print_double_line() {
+	eval printf %.0s= '{1..'"${COLUMNS}"\}
+	echo
+}
+
+print_line() {
+	eval printf %.0s- '{1..'"${COLUMNS}"\}
+	echo
+}
+
+########################################################################
+
+print_double_line
+CONDA_PREFIX=/cvmfs/northgrid.gridpp.ac.uk/simonsobservatory/conda/so-conda-py310-20240104
+. "${CONDA_PREFIX}/bin/activate"
+echo "Conda environment loaded with python available at:"
+which python
+export PATH="/cvmfs/northgrid.gridpp.ac.uk/simonsobservatory/usr/bin:$PATH"
+
+print_double_line
+echo "hostname: $(hostname)"
+
+print_double_line
+echo 'Debug info: env'
+env
+
+print_double_line
+echo 'Debug info: tree'
+tree -a ~
+
+print_double_line
+echo 'Debug info: df -Th'
+df -Th
+print_line
+echo 'Debug info: lsblk'
+lsblk
+
+print_double_line
+echo "Note that this package doesn't exist yet:"
+python -c 'import souk; print(souk.__file__)'
+
+print_double_line
+echo 'Installing souk from pip:'
+pip install --user souk
+
+print_double_line
+echo 'Note that this package now exists:'
+python -c 'import souk; print(souk.__file__)'
+which souk_arch_info
